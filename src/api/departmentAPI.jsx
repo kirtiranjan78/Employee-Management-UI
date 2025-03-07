32710882704ad319421ec56a6ecde36e97cef39e
kirtiranjan78 <kirtir279@gmail.com>
2025-03-07 18:45:26 +0530

    containerized with docker

--- src/api/departmentAPI.jsx
+++ src/api/departmentAPI.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8081/department";
+const API_URL = "http://localhost:2200/department";
 // Get list of departments
 export const getDepartments = () => {
   return axios.get(`${API_URL}/get`);
--- src/api/employeeAPI.jsx
+++ src/api/employeeAPI.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8081/employee",
+  baseURL: "http://localhost:2200/employee",
 });
 
 axiosInstance.interceptors.response.use(
